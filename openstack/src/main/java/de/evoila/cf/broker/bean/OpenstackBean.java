94efe19b8b5619c2ce82dbc3104015f69a6256df
René Schollmeyer <rschollmeyer@evoila.de>
2017-09-06 10:14:44 +0200

    feat: add implementation for the lbaas service broker

---
+++ openstack/src/main/java/de/evoila/cf/broker/bean/OpenstackBean.java
@@ -0,0 +1,21 @@
+package de.evoila.cf.broker.bean;
+
+import de.evoila.cf.broker.bean.impl.OpenstackBeanImpl.Cinder;
+import de.evoila.cf.broker.bean.impl.OpenstackBeanImpl.Project;
+import de.evoila.cf.broker.bean.impl.OpenstackBeanImpl.User;
+
+import java.util.List;
+
+public interface OpenstackBean {
+
+	public String getEndpoint();
+	public User getUser();
+	public Project getProject();
+	public String getNetworkId();
+	public String getSubnetId();
+	public String getImageId();
+	public String getKeypair();
+	public Cinder getCinder();
+	public String getPublicNetworkId();
+	
+}
